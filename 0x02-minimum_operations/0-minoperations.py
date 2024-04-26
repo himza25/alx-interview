b66701f4c969aa47c847cd08a1cbce39d8b17e3f
himza25 <himza.of.theking95@gmail.com>
2024-04-25 19:12:21 -0700

    task 3

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-def minOperations(n):
+"""Calculates minimum operations to reach n 'H' characters using "Copy All"
+and "Paste".
+"""
+
+
+def min_operations(n):
+    """Returns min number of operations to produce n"""
     if n < 2:
         return 0
     operations = 0
     current = n
-    for i in range(2, n + 1):
+    for i in range(2, int(n**0.5) + 1):
         while current % i == 0:
             operations += i
             current //= i
+    if current > 1:
+        operations += current
     return operations
 
 
 if __name__ == "__main__":
-    minOperations_test = __import__('0-minoperations').minOperations
     test_cases = [4, 12]
     for case in test_cases:
-        print(f"Min # of operations to reach {case} char: "
-              f"{minOperations_test(case)}")
+        print(f"Min # of operations to reach {case} characters: "
+              f"{min_operations(case)}")
