ed8c89dbb5ae00df78847a1e011a64cde90ceba6
himza25 <himza.of.theking95@gmail.com>
2024-05-23 06:44:32 -0700

    task completed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,48 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
+// Function to fetch character data
+function fetchCharacter(characterUrl, callback) {
+  request(characterUrl, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    callback(null, characterData.name);
+  });
+}
+
+// Fetch movie data
 request(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error fetching movie data:', error);
     return;
   }
   const filmData = JSON.parse(body);
   const characters = filmData.characters;
 
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error(charError);
-        return;
-      }
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
+  // Fetch character names in order
+  const fetchCharacterNames = characters.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      fetchCharacter(characterUrl, (error, name) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(name);
+        }
+      });
     });
   });
+
+  // Resolve all promises and print character names
+  Promise.all(fetchCharacterNames)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => {
+      console.error('Error fetching character names:', error);
+    });
 });
