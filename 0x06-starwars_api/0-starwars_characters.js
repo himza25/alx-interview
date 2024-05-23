a191ab0420f6ce9f5b4d52a1b7644ec2170c4e9c
himza25 <himza.of.theking95@gmail.com>
2024-05-23 06:55:22 -0700

    task completed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,6 +4,7 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
+// Function to fetch character data
 function fetchCharacter(characterUrl, callback) {
   request(characterUrl, (error, response, body) => {
     if (error) {
@@ -15,6 +16,7 @@ function fetchCharacter(characterUrl, callback) {
   });
 }
 
+// Fetch movie data
 request(url, (error, response, body) => {
   if (error) {
     console.error('Error fetching movie data:', error);
@@ -23,6 +25,7 @@ request(url, (error, response, body) => {
   const filmData = JSON.parse(body);
   const characters = filmData.characters;
 
+  // Fetch character names in order
   const fetchCharacterNames = characters.map(characterUrl => {
     return new Promise((resolve, reject) => {
       fetchCharacter(characterUrl, (error, name) => {
@@ -34,7 +37,8 @@ request(url, (error, response, body) => {
       });
     });
   });
-  
+
+  // Resolve all promises and print character names
   Promise.all(fetchCharacterNames)
     .then(names => {
       names.forEach(name => console.log(name));
