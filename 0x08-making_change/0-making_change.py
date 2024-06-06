0f4b36f9a8db25d065a0b44297483a47ea7ba599
himza25 <himza.of.theking95@gmail.com>
2024-06-06 16:19:18 -0700

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,7 +21,8 @@ def makeChange(coins, total):
 
     for coin in coins:
         for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)  # Update DP table
+            if dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)  # Update DP table
 
     return dp[total] if dp[total] != float('inf') else -1  # Return result
 
