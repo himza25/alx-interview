248508bf53dbee0c7006e463bd2b585b3d54f8b1
himza25 <himza.of.theking95@gmail.com>
2024-06-06 17:10:21 -0700

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,21 +3,29 @@
 Change comes from within
 """
 
-
-def help(start, end, coins, total, number, time):
+def help(start, end, coins, total, number, time, memo):
     """
-    backtracking recursion
-    that helps to find the number
+    Backtracking recursion with memoization
+    to find the number of coins.
     """
+    if (start, number) in memo:
+        return memo[(start, number)]
+    
     if start > end:
-        return -1
+        return float('inf')
+    
     while coins[start] + number <= total:
         number += coins[start]
         time += 1
+
     if number == total:
         return time
-    return help(start + 1, end, coins, total, number, time)
-
+    
+    include = help(start, end, coins, total, number, time, memo)
+    exclude = help(start + 1, end, coins, total, number, time, memo)
+    
+    memo[(start, number)] = min(include, exclude)
+    return memo[(start, number)]
 
 def makeChange(coins, total):
     """
@@ -27,5 +35,13 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+    
     sorted_coins = sorted(coins, reverse=True)
-    return help(0, len(sorted_coins) - 1, sorted_coins, total, 0, 0)
+    result = help(0, len(sorted_coins) - 1, sorted_coins, total, 0, 0, {})
+    return result if result != float('inf') else -1
+
+if __name__ == "__main__":
+    # Main file for testing
+    print(makeChange([1, 2, 25], 37))  # Output: 7
+    print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
+
