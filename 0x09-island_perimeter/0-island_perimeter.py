288d894667008085a62f2f6249cf9655ca799b41
himza25 <himza.of.theking95@gmail.com>
2024-06-12 11:20:03 -0700

    task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module for calculating the perimeter of an island in a grid.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list of list of ints): A list of lists representing the grid,
+        where 0 represents water and 1 represents land.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check all four directions around the land cell
+                if i == 0 or grid[i-1][j] == 0:  # Up
+                    perimeter += 1
+                if i == rows-1 or grid[i+1][j] == 0:  # Down
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:  # Left
+                    perimeter += 1
+                if j == cols-1 or grid[i][j+1] == 0:  # Right
+                    perimeter += 1
+
+    return perimeter
